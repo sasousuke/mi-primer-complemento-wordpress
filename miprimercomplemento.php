ac0da8039afd2427d44cb1300e953b5b972091a0
Ernesto Tur Laurencio <sasousuke@gmail.com>
2018-03-06 17:50:22 +0000

    Create miprimercomplemento.php
    Creando el archivo que contiene los datos que se muestran en la interface de plugins instalados de Wordpress

---
+++ miprimercomplemento.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Mi primer complemento
+Plugin URI: https://github.com/sasousuke/miprimercomplementowordpress
+Description: Mi primer complemento en Wordpress
+Version: 1.0.0
+Author: Ernesto Tur Laurencio
+Author URI: https://twitter.com/sasousuke
+*/
+?>
